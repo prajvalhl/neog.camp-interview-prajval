883857c572dcd6c61c27776708d9bb56b98868bf
Prajval <prajvalhl@gmail.com>
2021-01-30 12:48:54 +0530

    feat: added the output in the view and also shows in console like it previously did

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const age = document.getElementById("age");
+const output = document.getElementById("output");
 document.querySelector("#btn-submit").addEventListener("click", calcAge);
 function calcAge() {
   console.log(`Your current age is: ${2020 - Number(age.value)}`);
+  output.innerText = `Your current age is: ${2020 - Number(age.value)}`;
 }
