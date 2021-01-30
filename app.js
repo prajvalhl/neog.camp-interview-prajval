473b764a646f0c7b9804db48f41c8b57617c5565
Prajval <prajvalhl@gmail.com>
2021-01-30 12:53:06 +0530

    docs: added comments

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+// input
 const age = document.getElementById("age");
 const output = document.getElementById("output");
 document.querySelector("#btn-submit").addEventListener("click", calcAge);
+
+// processing and output
 function calcAge() {
   console.log(`Your current age is: ${2020 - Number(age.value)}`);
   output.innerText = `Your current age is: ${2020 - Number(age.value)}`;
